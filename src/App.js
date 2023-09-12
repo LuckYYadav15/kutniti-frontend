38db294f2db10a6e480f4d9b1be9ceba1526c699
LuckYYadav15 <20bcs077@iiitdwd.ac.in>
2023-09-12 23:44:40 +0530

    build files updated

--- src/App.js
+++ src/App.js
@@ -1,12 +1,8 @@
-import React from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-} from "react-router-dom";
-import { ToastContainer } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-import Home from './Pages/Home';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Home from "./Pages/Home";
 import Page404 from "./Pages/Page404";
 import CountryView from "./Pages/CountryView";
 import CountryDetail from "./Pages/CountryDetail";
@@ -15,31 +11,25 @@ import NewspaperDetail from "./Pages/NewspaperDetail";
 import About from "./Pages/About";
 import Methodology from "./Pages/Methodology";
 
-
 function App() {
   return (
+    <>
+      <Router>
+        <ToastContainer />
+        <Routes>
+          <Route path="/country-view" element={<CountryView />} />
+          <Route path="/country-detail" element={<CountryDetail />} />
+          <Route path="/newspaper-view" element={<NewspaperView />} />
+          <Route path="/newspaper-detail" element={<NewspaperDetail />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/methodology" element={<Methodology />} />
 
-<>
-<Router>
-<ToastContainer />
-<Routes>
-
-        <Route path="/country-view" element={<CountryView />} />
-        <Route path="/country-detail" element={<CountryDetail />} /> 
-        <Route path="/newspaper-view" element={<NewspaperView />} />
-        <Route path="/newspaper-detail" element={<NewspaperDetail />} /> 
-        <Route path="/about" element={<About />} /> 
-        <Route path="/methodology" element={<Methodology />} /> 
-        
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<Page404 />} />
-</Routes>
-</Router>
-</>
-)
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Page404 />} />
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
-
-
 export default App;
-
